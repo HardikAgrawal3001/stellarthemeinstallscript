03f00b10fa40d7b91d4e31c2cb1b0c6a793194ec
HardikAgrawal3001 <60972968+HardikAgrawal3001@users.noreply.github.com>
2024-02-18 14:54:09 +0530

    Use This Install Script To Install Theme

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
-
+cd /var/www/
+wget https://github.com/HardikAgrawal3001/stellarthemeinstallscript/raw/main/pterodactyl.zip
+unzip pterodactyl.zip
+cd /var/www/pterodactyl
+yarn add react-feather
+php artisan migrate
+yes
+yarn build:production
+php artisan view:clear
