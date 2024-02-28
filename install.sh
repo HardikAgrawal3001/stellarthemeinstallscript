be3db2a85af7d360c9368ad761740c36747709fb
HardikAgrawal3001 <60972968+HardikAgrawal3001@users.noreply.github.com>
2024-02-28 14:22:36 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,4 @@
 apt install npm
-y
 npm install --global yarn
 npm install -g n
 n 14.17.3
@@ -7,7 +6,6 @@ n prune
 cd /var/www/pterodactyl
 wget https://github.com/HardikAgrawal3001/stellarthemeinstallscript/raw/main/pterodactyl.zip
 unzip pterodactyl.zip
-A
 sudo yarn add react-feather
 php artisan migrate
 yes
